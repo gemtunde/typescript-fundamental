9be5558d790bbd81a2332db1beb2e12daa6bcf5f
gemtunde <gemtunde@gmail.com>
2023-02-03 10:30:18 +0100

    basic types

---
+++ basic-types.ts
@@ -0,0 +1,76 @@
+//intro
+//1.o data types for primitive data type
+
+//typecript ensures that data type dont change
+// its very strict with data type
+//e.g let price = 45
+//        price = 'chelsea'
+//it also ensures you dont just change or add your properties in an object
+//e.g  food = {
+//  orange : "yellow",
+//  size: "big"
+// }
+//food.amount = 890
+
+//implicit data type
+let myNumber = 3;
+// this will yield error becoz am assigning it another data-type of string => myNumber = 'excel'
+myNumber = 89;
+//same applies for strings and boolean too
+
+// explicit data-types
+let number: number = 45;
+let player: string = "excel";
+let man: boolean = true;
+//you cannot assign this values another data-type
+
+//with any, you can assign any data type
+let club: any = "chelsea";
+club = 79;
+club = true;
+
+//2.0 Non-primitive Data types e,g arrays and objects
+
+//impilcit data type
+const players = ["Ronaldo", "Messi", "Benzema", "Odegaard"];
+
+//explcit data type
+const myPlayers: string[] = ["Ronaldo", "Messi", "Benzema", "Odegaard"];
+
+// TS wouldnt allow to push number to the array. becos the array is a sting type => myPlayers.push(4);
+myPlayers.push("Salah");
+
+//define data types for functions parameters
+function sum(x: number, y: number) {
+  return x + y;
+}
+console.log(sum(8, 9));
+
+//define data types for returned valued in a functions
+function mySum(x: number, y: number): number {
+  return x + y;
+}
+console.log(sum(28, 89));
+
+//functions with object literals
+
+function add({ x, y }: { x: number; y: number }): number {
+  return x + y;
+}
+add({ x: 5, y: 8 });
+
+//use void type when your functions do not return anything
+
+function log(value: any): void {
+  console.log(value);
+}
+
+//functional parameter
+function optional(name?: string) {
+  if (name) {
+    console.log(`Tunde ${name}`);
+  } else {
+    console.log("Welcome");
+  }
+}
+optional("Welcome home");
